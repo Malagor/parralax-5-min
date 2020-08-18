a5b630a39b24ab62f52c76878522cffbea8f562a
Alex Malagor <seo.dnja@gmail.com>
2020-08-18 13:46:08 +0300

    Рабочий проект

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    var c1 = new Rellax('.parallax-item-1', {
+    speed: 5
+    });
+
+    var c2 = new Rellax('.parallax-item-2', {
+    speed: 8
+    });
+
+    var c3 = new Rellax('.parallax-item-3', {
+    speed: -4
+    });
+
+    var c4 = new Rellax('.parallax-item-4', {
+    speed: -7
+    });
+
+    });
